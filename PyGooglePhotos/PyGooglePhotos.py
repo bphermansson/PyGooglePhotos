e7d7c20ef35c1094319ca0a6d39ce681bb510a56
Patrik Hermansson <patrik@paheco.nu>
2016-05-23 16:53:01 +0200

    First working version

--- PyGooglePhotos/PyGooglePhotos.py
+++ PyGooglePhotos/PyGooglePhotos.py
@@ -1,60 +1,93 @@
 import sys
+import time
 from pydrive.auth import GoogleAuth 
 from pydrive.drive import GoogleDrive
-from PIL import Image
+from PIL import Image, ImageTk
+#import ImageTk
+from Tkinter import Tk, Label
 
-# Upload changes to Github:
-# git commit PyGooglePhotos.py
-# git push origin master
-# Update from Github
-# git pull
+"""
+Upload changes to Github:
+git commit PyGooglePhotos.py
+git push origin master
+Update from Github
+git pull
 
-#settings_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.yaml')
-settings_file = "settings.yaml"
+apt-get install python-imaging-tk
+pip2 install pillow
+"""
+def main():
 
-photos=[]
+	#settings_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'settings.yaml')
+	settings_file = "settings.yaml"
 
-gauth = GoogleAuth(settings_file=settings_file)
-gauth.CommandLineAuth()
-drive = GoogleDrive(gauth)
+	photos=[]
 
-# Auto-iterate through all files that matches this query
-file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+	gauth = GoogleAuth(settings_file=settings_file)
+	gauth.CommandLineAuth()
+	drive = GoogleDrive(gauth)
 
-print "List folders"
-for file1 in file_list:
-  #print 'title: %s, id: %s' % (file1['title'], file1['id'])
-  #print 'mime: %s' % (file1['mimeType'])	
-  if file1['mimeType'] == "application/vnd.google-apps.folder":
-	if file1['title'] == "Google Photos":
-		print "Found your photos:"
-		print 'Folder: %s, Id: %s' % (file1['title'],file1['id'])
-		found = True
+	# Auto-iterate through all files that matches this query
+	file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 
-if found == False:
-	print "Error, no Google Photos folder found"
-	sys.exit()
+	print "List folders"
+	for file1 in file_list:
+	  #print 'title: %s, id: %s' % (file1['title'], file1['id'])
+	  #print 'mime: %s' % (file1['mimeType'])	
+	  if file1['mimeType'] == "application/vnd.google-apps.folder":
+		if file1['title'] == "Google Photos":
+			print "Found your photos:"
+			print 'Folder: %s, Id: %s' % (file1['title'],file1['id'])
+			found = True
 
-# Google Photos folder id =1Q4Xk107tDLn4SdoHOqZwy5AGyvIm2bNO1uubxlMqx34
-# (found above)
-#folder_id="1Q4Xk107tDLn4SdoHOqZwy5AGyvIm2bNO1uubxlMqx34"
-#title: 2016, id: 1efl45Zs9_cFLEXzRERqqxIQp3Q
-folder_id="1efl45Zs9_cFLEXzRERqqxIQp3Q" # List files in folder 2016
+	if found == False:
+		print "Error, no Google Photos folder found"
+		sys.exit()
 
-_q = {'q': "'{}' in parents and trashed=false".format(folder_id)}
-photolist = drive.ListFile(_q).GetList()
-for file1 in photolist:
-	#print 'title: %s, id: %s' % (file1['title'], file1['id'])
-	#print 'mime: %s' % (file1['mimeType'])
-	if file1['mimeType'] == "image/jpeg":
-		imageurl = file1['title']
-		imageid =  file1['id']
-		#print 'title: %s' % (file1['title'])
-print "Open " + str(imageurl) + " id: " + str(imageid)
+	# Google Photos folder id =1Q4Xk107tDLn4SdoHOqZwy5AGyvIm2bNO1uubxlMqx34
+	# (found above)
+	#folder_id="1Q4Xk107tDLn4SdoHOqZwy5AGyvIm2bNO1uubxlMqx34"
+	#title: 2016, id: 1efl45Zs9_cFLEXzRERqqxIQp3Q
+	folder_id="1efl45Zs9_cFLEXzRERqqxIQp3Q" # List files in folder 2016
 
-# Download and show an image
-myFile = drive.CreateFile({'id': imageid})
-myFile.GetContentFile(imageurl)
-print '%s downloaded ' %(imageurl) + " and it looks like this:"
-image = Image.open(imageurl)
-image.show()
+	_q = {'q': "'{}' in parents and trashed=false".format(folder_id)}
+	photolist = drive.ListFile(_q).GetList()
+	for file1 in photolist:
+		#print 'title: %s, id: %s' % (file1['title'], file1['id'])
+		#print 'mime: %s' % (file1['mimeType'])
+		if file1['mimeType'] == "image/jpeg":
+			imageurl = file1['title']
+			imageid =  file1['id']
+			#print 'title: %s' % (file1['title'])
+	print "Open " + str(imageurl) + " id: " + str(imageid)
+
+	# Download and show an image
+	myFile = drive.CreateFile({'id': imageid})
+	myFile.GetContentFile(imageurl)
+	print '%s downloaded ' %(imageurl) + " and it looks like this:"
+	# Create fullscreen window
+	root = Tk() 
+	root.overrideredirect(True)
+	root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+	 
+	# Screen size?
+	screen_width = root.winfo_screenwidth()
+	screen_height = root.winfo_screenheight()
+	print str(screen_width) + "-" + str(screen_height)
+
+	image = Image.open(imageurl)
+	width, height = image.size
+	print str(width) + "-" + str(height)
+	image = image.resize((screen_width, screen_height), Image.ANTIALIAS) 
+	img = ImageTk.PhotoImage(image)
+	
+	panel = Label(root, image = img)
+	panel.pack(side = "bottom", fill = "both", expand = "yes")
+	root.mainloop()
+	time.sleep(10)
+if __name__ == "__main__":
+   try:
+      main()
+   except KeyboardInterrupt:
+      # do nothing here
+      sys.exit()
